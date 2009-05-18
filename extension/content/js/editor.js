c9752a200774705ee97fabba09d6c24cd5f59e46
Atul Varma <varmaa@toolness.com>
2009-05-18 00:34:12 -0700

    Bespin is now integrated into about:jetpack, though entering code into the editor still does nothing.

---
+++ extension/content/js/editor.js
@@ -0,0 +1,39 @@
+var JetpackCodeEditor = {
+  FILENAME: 'jetpack-editor-code.txt',
+  CHARSET: 'utf-8',
+
+  _component: null,
+
+  get fullPath() {
+    var file = DirIO.get('ProfD');
+    file.append(this.FILENAME);
+    return file;
+  },
+
+  initUI: function initUI(divId) {
+    var self = this;
+    // Loads and configures the objects that the editor needs
+    self._component = new bespin.editor.Component(divId, {
+      language: "js",
+      loadfromdiv: false
+      });
+    self._component.setContent(self.loadData());
+    self._component.onchange(function() {
+      self.saveData(self._component.getContent());
+      });
+  },
+
+  loadData: function loadData() {
+    var file = this.fullPath;
+    if (!file.exists())
+      return "";
+    return FileIO.read(file, this.CHARSET);
+  },
+
+  saveData: function saveData(data) {
+    var file = this.fullPath;
+    if (!file.exists())
+      FileIO.create(file);
+    FileIO.write(file, data, 'w', this.CHARSET);
+  }
+};
