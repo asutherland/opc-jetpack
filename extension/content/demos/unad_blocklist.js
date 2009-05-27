822b67de3d0bf24bd098b0155a7d5324b4ecad60
Atul Varma <varmaa@toolness.com>
2009-05-27 16:28:18 -0700

    Made the unad demo use jetpack.require() for all block-list processing functionality.

---
+++ extension/content/demos/unad_blocklist.js
@@ -0,0 +1,52 @@
+var rules = [];
+
+function ruleToRegExp(text) {
+  if ( text[0] == "!" || text[0] == "[" || text[0] == "@")
+    return null;
+  if ( text.match(/\$/) )
+    return null;
+
+  var regexp;
+
+  if (text[0] == "/" && text[text.length - 1] == "/") {
+    // filter is a regexp already
+    regexp = text.substr(1, text.length - 2);
+  } else {
+    // remove multiple wildcards
+    regexp = text.replace(/\*+/g, "*")
+      .replace(/(\W)/g, "\\$1")    // escape special symbols
+      .replace(/\\\*/g, ".*")      // replace wildcards by .*
+      .replace(/^\\\|/, "^")       // process anchor at expression start
+      .replace(/\\\|$/, "$")       // process anchor at expression end
+      .replace(/^(\.\*)/,"")       // remove leading wildcards
+      .replace(/(\.\*)$/,"");      // remove trailing wildcards
+  }
+
+  if (regexp == "")
+    return null;
+
+  return new RegExp(regexp);
+}
+
+function addRule( text ) {
+  var rule = ruleToRegExp(text);
+  if ( rule )
+    rules.push(rule);
+}
+
+exports.process = function process(data) {
+  data = data.split("\n");
+  for each ( line in data ) {
+    addRule( line );
+  }
+  addRule( "doubleclick" );
+};
+
+exports.match = function match( url ) {
+  for each ( rule in rules) {
+    if ( rule.exec(url) ) {
+      return true;
+    }
+  }
+  return false;
+};
