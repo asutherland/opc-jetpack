55d3e56cb02e3c387e66c2d17708e82d5697fc82
Atul Varma <varmaa@toolness.com>
2009-05-28 22:35:49 -0700

    Added docs for init-extension.js.

--- extension/content/js/init-extension.js
+++ extension/content/js/init-extension.js
@@ -1,6 +1,32 @@
+// = Extension Initialization =
+//
+// This module encapsulates a Firefox Extension in a chrome-privileged page.
+// This has a number of benefits:
+//
+// * The lifetime of the page is tied to the lifetime of the
+//   Extension: reloading the page effectively reloads the extension, which
+//   makes the process of developing an Extension much faster because
+//   the developer doesn't need to constantly restart their browser.
+//
+// * Scripts originally written for content space can be reused in
+//   chrome space without any modification (though obviously care must
+//   be taken to ensure that such scripts are secure).
+//
+// * Tools originally made for use with the Web, such as Firebug, can be
+//   reused in the context of Extension development.
+//
+// * With few exceptions, anything available to Web content is automatically
+//   available to the Extension's page, which isn't the case with JS Modules.
+//
+// The Extension's page is a ubiquitous singleton; when it's not loaded into
+// a browser window or tab, it's automatically loaded into a hidden window.
+
+// Globally alias the standard XPCOM accessors.
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 
+// Expose the MemoryTracking module to this page, so that objects can
+// be easily accounted for via weak references.
 Components.utils.import("resource://jetpack/modules/track.js");
 
 var Extension = {
@@ -21,6 +47,16 @@ var Extension = {
                .getService(Ci.nsIXULRuntime);
     return xulr.OS;
   },
+
+  // === {{{Extension.addUnloadMethod()}}} ===
+  //
+  // This attaches a given method called 'unload' to the given object.
+  // The method is also tied to the Extension page's lifetime, so if
+  // the unload method isn't called before the page is unloaded, it is
+  // called at that time.  This helps ensure both that memory leaks
+  // don't propagate past Extension page reloads, and it can also help
+  // developers find objects that aren't being properly cleaned up
+  // before the page is unloaded.
 
   addUnloadMethod: function addUnloadMethod(obj, unloader) {
     function unloadWrapper() {
