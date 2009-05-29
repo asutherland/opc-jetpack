f573a6018d1d77c032476651555d6f5b0bb1fa35
Atul Varma <varmaa@toolness.com>
2009-05-28 22:40:26 -0700

    Added docs for jquery-setup.js

--- extension/content/js/jquery-setup.js
+++ extension/content/js/jquery-setup.js
@@ -1,14 +1,16 @@
-// This sets up jQuery when it's loaded in a hidden chrome window
-// that doesn't provide a user interface.
+// = jQuery Setup =
+//
+// This sets up jQuery's Ajax functions to work when the host window
+// is loaded in a hidden window that doesn't provide a user interface.
+//
+// When the host window is invisible, we create the XHR object from
+// whatever current window the user's using, so that any UI that needs
+// to be brought up as a result of the XHR is shown to the user,
+// rather than being invisible and locking up the application.
 
 jQuery.ajaxSetup(
   {xhr: function() {
-     // This is a fix for Ubiquity bug #470. We're going to create the
-     // XHR object from whatever current window the user's using, so
-     // that any UI that needs to be brought up as a result of the XHR
-     // is shown to the user, rather than being invisible and locking
-     // up the application.
-
+     // This is a fix for Ubiquity bug #470.
      if (Extension.isHidden) {
        var jsm = {};
        Components.utils.import("resource://jetpack/ubiquity-modules/utils.js", jsm);
