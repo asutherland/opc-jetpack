dedf0af06a38a1e4f147801201725a420f77ac80
Edward Lee <edilee@mozilla.com>
2009-07-08 15:29:23 -0700

    Move SlideBar and Selection demos to website.
    --HG--
    rename : extension/content/demos/selection-install.html => website/demos/selection-install.html
    rename : extension/content/demos/selection.js => website/demos/selection.js
    rename : extension/content/demos/videoSlide-install.html => website/demos/videoSlide-install.html
    rename : extension/content/demos/videoSlide.js => website/demos/videoSlide.js

--- website/demos/videoSlide.js
+++ website/demos/videoSlide.js
@@ -1,23 +1,38 @@
 jetpack.future.import("slideBar");
-jetpack.slideBar.append({
-  // Listen for click events on the icon
-  onReady: function(slide) $(slide.icon).click(function() {
-    let hasVideos = function() $(slide.doc).find("div.video").length > 1;
 
+// Keep track of how many videos moved to the SlideBar
+let videoCount = 0;
+
+// Get video-like elements from the currently focused tab
+function getTabVideos() {
+  return $(jetpack.tabs.focused.contentDocument).find("video, embed");
+}
+
+jetpack.slideBar.append({
+  onReady: function(slider) {
+    // Listen for tab-ready events and notify in the SlideBar if there's videos
+    jetpack.tabs.onReady(function() {
+      if (getTabVideos().length > 0)
+        slider.notify();
+    });
+  },
+  
+  onClick: function(slider) {
     // Find video elements to put in the SlideBar
-    $(jetpack.tabs.focused.contentDocument).find("video, embed").each(function() {
+    getTabVideos().each(function() {
       let paused = this.paused;
 
-      // Move the video to the slidebar
-      let video = $(slide.doc).find("proto > .video").clone();
-      video.appendTo(slide.doc.body).prepend(this);
+      // Move the video to the SlideBar
+      let video = $(slider.contentDocument).find("proto > .video").clone();
+      video.appendTo(slider.contentDocument.body).prepend(this);
+      ++videoCount;
 
       // Add a control to remove the video
       video.find(".remove").click(function() {
         video.remove();
-        // No more videos? Hide the slidebar
-        if (!hasVideos())
-          slide();
+        // No more videos? Close the slidebar
+        if (--videoCount == 0)
+          slider.close();
       });
 
       // Show built-in firefox video controls; we lose the site's custom ones
@@ -29,18 +44,18 @@ jetpack.slideBar.append({
     });
 
     // Slide open to show videos, but only stay open if we have videos
-    slide({ size: 206, persist: hasVideos() });
-  }),
-  
-  width: 206,
+    if (videoCount > 0)
+      slider.slide(this.width, true);
+  },
 
+  width: 200,
   html: <>
     <style><![CDATA[
-      body { margin: 0; }
+      body { margin: 0; overflow: hidden; }
       video, embed { max-height: 150px; width: 200px; }
       help { font-style: italic; }
       proto { display: none; }
-      div.video { margin: 3px; min-height: 2em; position: relative; }
+      div.video { margin: 3px 0; min-height: 2em; position: relative; }
       div.video div.remove { background: #000; color: #fff; height: 1em; left: 3px; position: absolute; top: 3px; visibility: hidden; width: 1em; }
       div.video:hover div.remove { border: 1px solid #fff; -moz-border-radius: 1em; opacity: .6; text-align: center; visibility: visible; }
       div.video div.remove:hover { cursor: pointer; opacity: .8; }
