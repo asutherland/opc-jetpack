1f636a63282155a1c428b79978b9c037298fe6ea
Anant Narayanan <anant@kix.in>
2009-07-10 16:33:37 -0700

    Merge audio module to existing Makefile
    --HG--
    extra : rebase_source : fbe811ae8a3cfde1d93f7f4e3ea8bf61dfad3d0e

--- components/src/jetpackmodule.cpp
+++ components/src/jetpackmodule.cpp
@@ -1,10 +1,33 @@
 #include "nsIGenericFactory.h"
 #include "nsJSWeakRef.h"
 
+#ifdef BUILD_AUDIO
+#include "AudioRecorder.h"
+#include "AudioEncoder.h"
+
+NS_GENERIC_FACTORY_CONSTRUCTOR(AudioEncoder)
+NS_GENERIC_FACTORY_CONSTRUCTOR(AudioRecorder)
+#endif
+
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsJSWeakRef)
 
 static nsModuleComponentInfo components[] =
 {
+#ifdef BUILD_AUDIO
+    {
+        AUDIO_RECORDER_CLASSNAME,
+        AUDIO_RECORDER_CID,
+        AUDIO_RECORDER_CONTRACTID,
+        AudioRecorderConstructor,
+    },
+
+    {
+    	AUDIO_ENCODER_CLASSNAME,
+    	AUDIO_ENCODER_CID,
+    	AUDIO_ENCODER_CONTRACTID,
+    	AudioEncoderConstructor,
+    },
+#endif
     {
         NSJSWEAKREFDI_CLASSNAME,
         NSJSWEAKREFDI_CID,
