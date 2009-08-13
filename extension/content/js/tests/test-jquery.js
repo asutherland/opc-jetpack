e0ef535dc2c73512bec072e949698e0b450349bc
Atul Varma <varmaa@toolness.com>
2009-08-12 22:56:34 -0700

    Added a new 'testJqueryEventHandlersAreUnloaded' test.

--- extension/content/js/tests/test-jquery.js
+++ extension/content/js/tests/test-jquery.js
@@ -1,4 +1,29 @@
 var JqueryTests = {
+  testJqueryEventHandlersAreUnloaded: function(self) {
+    var jqsb = JQuerySandbox.create(window);
+    var div = $('<div></div>');
+    var divElem = div.get(0);
+    var timesCalled = 0;
+
+    $(document.body).append(div);
+    jqsb.$(divElem).click(function() { timesCalled++; });
+
+    function doClick() {
+      var evt = document.createEvent("MouseEvents");
+      evt.initMouseEvent("click", true, true, window,
+                         0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      divElem.dispatchEvent(evt);
+    }
+
+    doClick();
+    self.assertEqual(timesCalled, 1);
+    jqsb.unload();
+    doClick();
+    self.assertEqual(timesCalled, 1);
+
+    div.remove();
+  },
+
   testJqueryCanBeLoadedInSandbox: function(self) {
     var jqsb = JQuerySandbox.create("http://www.foo.com");
 
