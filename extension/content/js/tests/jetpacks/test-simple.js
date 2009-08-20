c9c6de6078eda9473be540abd2974d88230fa8c0
Atul Varma <varmaa@toolness.com>
2009-08-20 10:59:49 -0700

    Added more to test-simple.js.
    --HG--
    extra : rebase_source : 528943ee65555eee8cf5e0d1bd51be5a14e8d023

--- extension/content/js/tests/jetpacks/test-simple.js
+++ extension/content/js/tests/jetpacks/test-simple.js
@@ -1,1 +1,12 @@
 test.assert(jetpack.tabs.focused.contentWindow);
+test.assert(jetpack.lib.twitter);
+test.assert(jetpack.notifications.show);
+test.assert(jetpack.tabs.focused);
+test.assert(jetpack.statusBar.append);
+test.assert(jetpack.track);
+test.assert(jetpack.storage.live);
+test.assert(JSON.parse);
+test.assert(JSON.stringify);
+test.assert(setInterval);
+test.assert(clearTimeout);
+test.assert(new XMLHttpRequest());
