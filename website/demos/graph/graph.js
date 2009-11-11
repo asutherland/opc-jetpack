356c6f0f327bf45cefac1da7ab65f68731013cba
aza@aza-raskins-macbook-air.local <aza@aza-raskins-macbook-air.local>
2009-11-10 20:24:50 -0800

    Updated a number of the demos:
    - Updated the visual style of gmail checker
    - Updated the functionality and look of the tab grapher
    - Updated the size of the clipboard demo

--- website/demos/graph/graph.js
+++ website/demos/graph/graph.js
@@ -6,35 +6,46 @@
 	@license: MPL
 */
 
+// TODO: This only works when there is one window!
+// We need the Shadow DOM to fix this properly.
+
 // We keep the history in session storage so that the sparkline
 // is maintained even during development.
 
 var stash = jetpack.storage.live;
 if( !stash.history ) stash.history = [jetpack.tabs.length]
 
-var data = null;
-var legend = null;
-
-function updateGraph(){
-  stash.history.push( jetpack.tabs.length );
-
-  if( stash.history.length > 400 )
-    stash.history = stash.history.slice( 1 );
-
-  data.text( stash.history.join(",") );
-  legend.text( stash.history[ stash.history.length-1 ] ); 
-}
-
-jetpack.tabs.onOpen( updateGraph );
-jetpack.tabs.onClose( updateGraph );
-jetpack.tabs.onFocus( updateGraph );
-
 jetpack.statusBar.append({
   url: "statusbar.html",
-  width: 175,
   onReady: function(widget){
     data = $("#data", widget);
     legend = $("#legend", widget);
+    
+    function updateGraph(){
+      stash.history.push( jetpack.tabs.length );
+
+      if( stash.history.length > 400 )
+        stash.history = stash.history.slice( 1 );
+
+      data.text( stash.history.join(",") );
+      legend.text( stash.history[ stash.history.length-1 ] ); 
+    }
+
+    jetpack.tabs.onOpen( updateGraph );
+    jetpack.tabs.onClose( updateGraph );
+    setInterval( updateGraph, 1000*60*2 ); // Flat-line every three minutes.
+    
     updateGraph();
+    
+    jetpack.future.import("menu");
+    jetpack.menu.context.set({
+      label: "Reset",
+      command: function(){
+        stash.history = [ jetpack.tabs.length ];
+        updateGraph();
+      }
+    });
+    
+    
   }
 })
--- website/demos/trim.js
+++ website/demos/trim.js
@@ -94,7 +94,6 @@ var theHTML = "<html><head><style>body{text-align: center} #trim{ color: #666; f
 
 jetpack.statusBar.append({
   html: theHTML,
-  width: 50,
   onReady: function(doc) {
     var trim = new Trim(doc);
   }
