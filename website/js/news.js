3058c670067c1647494142b14567e77e3c9a7c45
Daniel Buchner <dbuchner@mozilla.com>
2010-01-08 13:41:35 -0800

    Changes committed by Atul on Daniel's behalf.

--- website/js/news.js
+++ website/js/news.js
@@ -2,9 +2,8 @@
 
 var messages = [
   'Take the <a href="http://design-challenge.mozillalabs.com/jetpack-for-learning/">Jetpack for Learning Design Challenge</a> to advanced online learning.',
-  'Enter the <a href="http://mozillalabs.com/jetpack/2009/11/13/jetpack-50-line-code-challenge/">Jetpack 50-line Challenge</a>. Win a netbook.',
-  '<a href="http://mozillalabs.com/blog/2009/11/announcing-jetpack-0-6-jetpack-gallery/">Jetpack 0.6</a> adds two major APIs.</li>',
-  'Jetpack 0.6.2 just released. Brings the built-in Bespin editor to Windows and Linux.',
+  'Enter the <a href="http://mozillalabs.com/blog/2010/01/jetpack-50-line-code-challenge-winner-is-crowned/">Jetpack 50-line Challenge Winner Announced!</a>',
+  '<a href="http://mozillalabs.com/blog/2009/11/announcing-jetpack-0-6-jetpack-gallery/">Jetpack 0.7</a> adds a First-Run experience API.</li>',
   'The <a href="http://jetpackgallery.mozillalabs.com">Jetpack Gallery</a> is now up! Add your Jetpack for maximum exposure.'
 ];
 
